fed66b8c4ca1d03def4b30f921d5d3df8a05d30b
SokolovG <griso2010@yandex.ru>
2025-06-04 22:53:54 +0300

    critical auth service bugf fixed

--- src/services/user_service.py
+++ src/services/user_service.py
@@ -1,10 +1,15 @@
 from typing import Any
 
+from litestar import Request
 from litestar_users.service import BaseUserService
 
 from src.database.models import User
 
 
-class UserService(BaseUserService[User, Any, Any]):  # type:ignore
-    async def post_registration_hook(self, user: User) -> None:  # type:ignore
+class UserService(BaseUserService[User, Any, Any]):  # type: ignore
+    async def post_registration_hook(self, user: User, request: Request) -> None:  # type: ignore
+        """Hook called after successful user registration."""  # noqa: D401
         print(f"User <{user.email}> has registered!")
+        # - отправка welcome email
+        # - создание профиля пользователя
+        # - логирование события
