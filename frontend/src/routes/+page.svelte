356e74a30a7fb4fa882e77cc55157fb5bea715d5
SokolovG <griso2010@yandex.ru>
2025-07-10 23:19:00 +0300

    type updated

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -15,7 +15,11 @@
     <div>
         <button on:click={loadRecords}>Load records</button>
         {#each records as record}
-            <p>record.title</p>
+            <p> ID: {record.id}</p>
+            <p> Title: {record.title}</p>
+            <p> Raw data: {record.raw_input}</p>
+            <p> AI processed: {record.is_processed}</p>
+            <p> Approved: {record.is_approved}</p>
         {/each}
     </div>
 </main>
