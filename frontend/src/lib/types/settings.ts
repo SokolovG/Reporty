716d3ba0840da9776aa3a86a980a537d6a4b9a1d
SokolovG <griso2010@yandex.ru>
2025-09-23 22:05:23 +0200

    added types, rename settings_controller

--- frontend/src/lib/types/settings.ts
+++ frontend/src/lib/types/settings.ts
@@ -1,3 +1,4 @@
 export interface TaskType {
     title: string
+    color: string
 }
--- frontend/src/lib/types/user.ts
+++ frontend/src/lib/types/user.ts
@@ -1,4 +1,12 @@
 export interface User {
+    username: string
     email: string
     password: string
+    display_name: string
+    department: string
+    position: string
+    ai_auto_process: boolean
+    ai_provider_id: number
+    is_active: boolean
+    is_verify: boolean
 }
